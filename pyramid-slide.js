2e51a5a01a8d4a364c5b93a48a6cfd28d499be51
Carly <cj.langlois.cl@gmail.com>
2016-10-13 18:18:38 -0500

    pyramid-slide submission

---
+++ pyramid-slide.js
@@ -0,0 +1,50 @@
+document.getElementById("draw-form").oninput = function() {
+
+    // figure out the height the user typed
+    heightStr = document.getElementById("height").value
+
+    // convert the string to an int
+    height = parseInt(heightStr);
+
+    // draw pyramid with that height
+    drawPyramid(height);
+}
+
+
+/**
+ * drawPyramid
+ *
+ * Renders, in the HTML document, a Mario pyramid of the specified height
+ */
+ function drawPyramid(height) {
+
+     // first, clear the old content
+     document.getElementById("pyramid").innerHTML = "";
+
+     // for each row....
+     for (var row = 0; row < height; row++) {
+
+         // figure out number of bricks and spaces
+         var numBricks = row + 2;
+         var numSpaces = height - row - 1;
+
+         // build up a string for this row
+         var rowStr = "";
+         for (var i = 0; i < numSpaces; i++) {
+             rowStr += "\u00A0";
+         }
+         for (var i = 0; i < numBricks; i++) {
+             rowStr += "#";
+         }
+
+        // create a text element with the string of characters
+        textElem = document.createTextNode(rowStr);
+
+        // create a <p> element with the text inside
+        rowElem = document.createElement("p");
+        rowElem.appendChild(textElem);
+
+        // insert the paragraph as a child of the container <div>
+        document.getElementById("pyramid").appendChild(rowElem);
+    }
+}
